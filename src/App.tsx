037a3cd8cfcbf656856b5e86414553a8ab9c7715
David Bassett <112457273+Yylium@users.noreply.github.com>
2024-03-08 22:55:22 -0700

    fix things

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,20 @@ const teams = require('./CollegeBasketballTeams.json');
 
 
 // The header introducing the website.
-function Header()
-{
+function Header() {
     return (
         <>
-            <br /><br /><br />
-            <h2>This is a website for listing information about all the colleges in NCAA Basketball.</h2>
-            <br /><br /><br />
-            <br /><br /><br />
+            <br/><br/><br/>
+            <h2>This is a website for listing information about all the colleges in NCAA
+                Basketball.</h2>
+            <br/><br/><br/>
+            <br/><br/><br/>
         </>
     )
 }
 
 // An individual card representing a single NCAA team.
-function TeamCard(team: { school: string; name: string; city: string; state: string;}) {
+function TeamCard(team: { school: string; name: string; city: string; state: string; }) {
 
 
     return (
@@ -36,24 +36,30 @@ function CardList() {
     return (
         <>
             {/*Take each team from the json object and pass the relevant properties into the TeamCard component.*/}
-            {teams.teams.map((team: {school: string; name: string; city: string; state: string}) => {
+            {teams.teams.map((team: {
+                school: string;
+                name: string;
+                city: string;
+                state: string
+            }) => {
                 return (
 
-                    <TeamCard school={team.school} name = {team.name} city={team.city} state={team.state} />
-                )})}
+                    <TeamCard school={team.school} name={team.name} city={team.city}
+                              state={team.state}/>
+                )
+            })}
 
         </>
     )
-
-
+}
 
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <Header />
-                <CardList />
+                <Header/>
+                <CardList/>
             </header>
         </div>
     );
